a23eff836570a83225cab7373cd29b74eb2fde82
Zain Subhani <zainsubhani@Zains-MacBook-Pro.local>
2026-03-31 17:30:11 +0200

    added form component 34

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -29,6 +29,7 @@ import {
   InputGroupText,
   InputGroupTextarea,
 } from "@/components/ui/input-group"
+import Image from 'next/image';
 
 
 const AuthForm = () => {
@@ -39,96 +40,53 @@ const AuthForm = () => {
       description: "",
     },
   })
- 
+
   function onSubmit(data: z.infer<typeof formSchema>) {
     // Do something with the form values.
     console.log(data)
   }
 
   return (
-    <div>
-       <Card className="w-full sm:max-w-md">
-      <CardHeader>
-        <CardTitle>Bug Report</CardTitle>
-        <CardDescription>
-          Help us improve by reporting bugs you encounter.
-        </CardDescription>
-      </CardHeader>
-      <CardContent>
-        <form id="form-rhf-demo" onSubmit={form.handleSubmit(onSubmit)}>
-          <FieldGroup>
-            <Controller
-              name="title"
-              control={form.control}
-              render={({ field, fieldState }) => (
-                <Field data-invalid={fieldState.invalid}>
-                  <FieldLabel htmlFor="form-rhf-demo-title">
-                    Bug Title
-                  </FieldLabel>
-                  <Input
-                    {...field}
-                    id="form-rhf-demo-title"
-                    aria-invalid={fieldState.invalid}
-                    placeholder="Login button not working on mobile"
-                    autoComplete="off"
-                  />
-                  {fieldState.invalid && (
-                    <FieldError errors={[fieldState.error]} />
-                  )}
-                </Field>
-              )}
-            />
-            <Controller
-              name="description"
-              control={form.control}
-              render={({ field, fieldState }) => (
-                <Field data-invalid={fieldState.invalid}>
-                  <FieldLabel htmlFor="form-rhf-demo-description">
-                    Description
-                  </FieldLabel>
-                  <InputGroup>
-                    <InputGroupTextarea
-                      {...field}
-                      id="form-rhf-demo-description"
-                      placeholder="I'm having an issue with the login button on mobile."
-                      rows={6}
-                      className="min-h-24 resize-none"
-                      aria-invalid={fieldState.invalid}
-                    />
-                    <InputGroupAddon align="block-end">
-                      <InputGroupText className="tabular-nums">
-                        {field.value.length}/100 characters
-                      </InputGroupText>
-                    </InputGroupAddon>
-                  </InputGroup>
-                  <FieldDescription>
-                    Include steps to reproduce, expected behavior, and what
-                    actually happened.
-                  </FieldDescription>
-                  {fieldState.invalid && (
-                    <FieldError errors={[fieldState.error]} />
-                  )}
-                </Field>
-              )}
-            />
-          </FieldGroup>
-        </form>
-      </CardContent>
-      <CardFooter>
-        <Field orientation="horizontal">
-          <Button type="button" variant="outline" onClick={() => form.reset()}>
-            Reset
-          </Button>
-          <Button type="submit" form="form-rhf-demo">
-            Submit
-          </Button>
-        </Field>
-      </CardFooter>
-    </Card>
-  
+    <div className='card-border  lg:min-w-139 '>
+      <div className='flex flex-col gap-6 card py-14 px-10 '>
+        <div className=' flex flex-row gap-2 justify-center  ' >
+          <Image src="/logo.svg" alt="Logo" width={32} height={38} />
+          <h2 className='text-primary-100 '>
+            Interview Prep Platform
+          </h2>
+
+        </div>
+        <h3>Form Platform</h3>
+
+      </div>
+      <div className='w-full '>
+          <Card className="w-full sm:max-w-md">
+       
+        <CardContent>
+          <form id="form-rhf-demo"  onSubmit={form.handleSubmit(onSubmit)}>
+            <FieldGroup>
+              
+             
+            </FieldGroup>
+          </form>
+        </CardContent>
+        <CardFooter>
+          <Field orientation="horizontal">
+            <Button type="button" variant="outline" onClick={() => form.reset()}>
+              Reset
+            </Button>
+            <Button type="submit" form="form-rhf-demo">
+              Submit
+            </Button>
+          </Field>
+        </CardFooter>
+      </Card>
+      </div>
+    
+
 
     </div>
-  
+
   )
 }
 
